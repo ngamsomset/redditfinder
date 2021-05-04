1e18ea0ff6207d009bdf6a6400419010520a451b
Setthasiri Ngamsom <ngamsomset@setthasiris-mbp.home>
2021-05-04 12:30:14 +1000

    fetch

--- redditapi.js
+++ redditapi.js
@@ -1,7 +1,7 @@
 export default {
   search: function (searchTerm, searchLimit, sort) {
     return fetch(
-      `http://www.reddit.com/search.json?q=${searchTerm}&sort=${sort}&limit=${searchLimit}`
+      `https://www.reddit.com/search.json?q=${searchTerm}&sort=${sort}&limit=${searchLimit}`
     )
       .then((res) => res.json())
       .then((data) => data.data.children.map((data) => data.data))
